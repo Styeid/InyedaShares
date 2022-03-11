4f331e316c8df5d6ee33551b674ae84e7c7190d4
Styeid <88354740+Styeid@users.noreply.github.com>
2022-03-11 02:56:31 +0100

    commit 06-03-2022, building Layout totaal commit 10-03-2022, building layout data-refresh

---
+++ app/src/main/java/com/jufaja/inyedashares/ARefreshDataActivity.kt
@@ -0,0 +1,19 @@
+package com.jufaja.inyedashares
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+
+private const val TAG = "ARefreshDataActivity"
+class ARefreshDataActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.a_activity_refresh_data)
+    }
+
+
+}
